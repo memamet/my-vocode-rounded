1ad5d26dc09851a7027b29f00e0853fe1b3aa63d
max-eugene@hotmail.fr <max-eugene@hotmail.fr>
2024-10-31 18:10:22 +0100

    style: add titles and pictures on the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,77 +1,106 @@
-'use client'
+/* eslint-disable react/no-unescaped-entities */
+"use client";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
-import dynamic from 'next/dynamic';
-import Image from 'next/image'
+import dynamic from "next/dynamic";
+import Image from "next/image";
 
-const VocodeAppDynamic = dynamic(() => import('@/components/vocode-app'), { ssr: false });
-
+const VocodeAppDynamic = dynamic(() => import("@/components/vocode-app"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex justify-end">
-            <a
-              href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvocodedev%2Fvocode-next-template&demo-title=Next.js%2FVercel%20Template%20with%20Vocode's&demo-description=The%20fastest%20way%20to%20build%20Voice%20AI%20application%20with%20Vocode%20and%20Next.js&demo-image=https%3A%2F%2Fraw.githubusercontent.com%2Fvocodedev%2Fvocode-next-template%2Fmain%2Fpublic%2Freadme_hero.webp"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                src="/deploy.svg"
-                alt="Deploy with Vercel"
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-            <a
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-              href="https://docs.vocode.dev?utm_source=vocode-next-vercel-template&utm_medium=vocode-next-vercel-template&utm_campaign=vocode-next-vercel-template"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              <Image
-                src="/vocode.svg"
-                alt="Vocode Logo"
-                className="dark:invert"
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-900 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <a
+          href="https://rounded.com/contact"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="p-4"
+        >
+          <Image
+            src="/rounded_logo.svg"
+            alt="Rounded Logo"
+            width={100}
+            height={24}
+            priority
+          />
+        </a>
+      </div>
+      <div className="relative flex place-items-center z-[-1]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/vocode.svg"
-          alt="Vocode Logo"
+          className="relative "
+          src="/donny_logo.avif"
+          alt="Donny, the Rounded AI Assistant"
           width={280}
           height={37}
           priority
         />
-        <a className="text-2xl px-6">/</a>
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next Logo"
-          width={180}
-          height={37}
-          priority
-        />
       </div>
-      <p className="text-lg text-center font-semibold dark:text-white">
-        The fastest way to build Voice AI application with 
-        <a href="https://vocode.dev" target="_blank" rel="noopener noreferrer" className="text-blue-800 hover:underline dark:text-blue-300 dark:hover:underline"> Vocode</a> and 
-        <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer" className="text-blue-800 hover:underline dark:text-blue-300 dark:hover:underline"> Next.js</a>
+      <h1 className="text-3xl font-semibold text-center dark:text-white my-8">
+        Meet Donny – Your Expert Guide to Rounded
+      </h1>
+      <p className="text-lg text-center font-semibold dark:text-white mb-8">
+        Donny is here to help you learn everything about Rounded's journey,
+        products, and innovations.
       </p>
       <div className="flex flex-col items-center justify-center py-4">
-        <VocodeAppDynamic defaultBackendUrl={(window.location.protocol === 'https:' ? 'wss:' : 'ws:') + "//" + window.location.host + "/api/python/conversation"} isInputEditable={false} />
+        <VocodeAppDynamic
+          defaultBackendUrl={
+            (window.location.protocol === "https:" ? "wss:" : "ws:") +
+            "//" +
+            window.location.host +
+            "/api/python/conversation"
+          }
+          isInputEditable={false}
+        />
       </div>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="grid text-center lg:max-w-5xl lg:w-full lg:grid-cols-4 lg:text-left gap-4">
+        <div>
+          <h2 className="text-2xl font-bold dark:text-white mb-4">
+            About Rounded
+          </h2>
+          <p className="text-md dark:text-white">
+            Rounded was founded to transform customer interactions with
+            AI-driven callbots, starting in healthcare. From patient scheduling
+            to scalable voice solutions, Rounded is building the future of
+            automated customer service.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-2xl font-bold dark:text-white mb-4">
+            Our Founders
+          </h2>
+          <p className="text-md dark:text-white">
+            Rounded was created by Aymeric Vaudelin, Valentin Flageat, and
+            Yassine M'hamdi, who met at the X-HEC Entrepreneurs program. Their
+            mission: to make customer interactions more efficient, scalable, and
+            engaging.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-2xl font-bold dark:text-white mb-4">
+            Voice Agent Factory
+          </h2>
+          <p className="text-md dark:text-white">
+            The Voice Agent Factory is Rounded’s flagship tool, enabling
+            companies to build custom AI voice agents that seamlessly integrate
+            with business operations, no matter the industry.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-2xl font-bold dark:text-white mb-4">
+            International Growth
+          </h2>
+          <p className="text-md dark:text-white">
+            Rounded has expanded globally, with support from programs like
+            Skydeck (California) and Station F (France), bringing innovative
+            voice solutions to customers worldwide.
+          </p>
+        </div>
       </div>
-
     </main>
   );
 }
