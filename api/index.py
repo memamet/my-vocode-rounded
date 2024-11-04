531dcfa10f452fdcf4c85c80936dfa35f2a9ff74
max-eugene@hotmail.fr <max-eugene@hotmail.fr>
2024-11-04 11:49:53 +0100

    dockerize full dev environment

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ import os
 import logging
 from fastapi import FastAPI, WebSocket, Query
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 import datetime
 import json
 
@@ -23,6 +24,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://frontend:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
